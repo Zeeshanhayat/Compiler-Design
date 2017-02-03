e300f70af560a7506795392ffaf321dd88d5c42c
zeeshanhayat <malikzeeshanhayat@yahoo.com>
2017-02-03 14:22:27 +0100

    remove lab3

--- Lab3/CS6241_HW2-master/README.md
+++
@@ -1,4 +0,0 @@
-CS6241_HW2
-==========
-
-Homework 2 for CS6241 (Compiler Design) at Georgia Tech.
--- Lab3/CS6241_HW2-master/unusedVariable.c
+++
@@ -1,6 +0,0 @@
-int main() {
-    int i = 6;
-    int k = 8;
-    int j = k + 4;
-    return k;
-}
